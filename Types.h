3573b5e48c2236d493b4e0d0b2b5d0d3d3e8ab38
Mikael Alves Win <mikaelalvez5@gmail.com>
2025-06-03 15:22:01 -0300

    fix: motores

---
+++ Types.h
@@ -0,0 +1,12 @@
+#ifndef DXUT_TYPES_H
+#define DXUT_TYPES_H
+
+// atalhos para tipos da linguagem C++
+using uchar  = unsigned char;
+using ushort = unsigned short;
+using ulong  = unsigned long;
+using llong  = long long;
+using ullong = unsigned long long;
+using uint   = unsigned int;
+
+#endif
